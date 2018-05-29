54533e887dec9ef039a44410f80022fdcb3dfe50
PhilHeek <philippe@heek.com>
2018-05-29 21:50:53 +0200

    removing useless folder from the repo

--- dist/routes/router.js
+++
@@ -1,9 +0,0 @@
-var _establishment = require('./establishment');
-
-var _establishment2 = _interopRequireDefault(_establishment);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-module.exports = function (app) {
-  (0, _establishment2['default'])(app);
-};
